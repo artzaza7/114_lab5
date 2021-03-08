5dd47466a1bebd81d4d2737642745953aa5bef65
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-03-08 18:17:00 +0700

    lab5_3 ผิดพลาด

--- lab5_3.c
+++ lab5_3.c
@@ -3,13 +3,13 @@ int main()
 {
     long long int num;
     scanf("%lld",&num);
-    long long int i=1,number=2,make[1000000000000]={0};
+    long long int i=1,number=2,make[10000000000]={0};
     if(num==1)
     {
         printf("1");
     }
     else{
-    while(number<=1000000000000)
+    while(number<=10000000000)
     {
         if(number%2==0||number%3==0||number%5==0)
             make[i]=number;
